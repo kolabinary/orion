32e5a6ac89d688062395ceee2ad6edb6b3001668
kolabinary <awi.ifeoluw@gmail.com>
2020-07-16 00:42:54 +0100

    landing-page

--- orion/src/App.vue
+++ orion/src/App.vue
@@ -1,27 +1,34 @@
 <template>
   <div id="app">
 
+    <home/>
+
   </div>
 </template>
 
 <script>
 
+  import home from  './components/home.vue'
+
 
 export default {
   name: 'App',
   components: {
-
+          home
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Nunito Sans', sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
+  padding: 0;
+  margin: 0!important;
 }
 </style>
---
+++ orion/src/components/home.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container">
+    <nav class="header">
+      <div><img src="../assets/img/orion-logo.svg" alt=""></div>
+      <div><img src="../assets/img/menu.svg" alt=""></div>
+    </nav>
+
+    <section class="hero">
+      <div><h6 style="color: #fff;">Welcome to Orion 5</h6></div>
+      <div><h1><span style="color: #7842F0;">no</span> gravity</h1></div>
+    </section>
+
+    <section class="halo">
+      <a href="#">WARP DRIVE</a>
+    </section>
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+
+}
+</script>
+
+
+
+<style scoped lang="scss">
+
+  .container {
+    color: #fff;
+    background-color:#000;
+    height: calc(100vh);
+    padding: 0 6%;
+
+    .header {
+      padding: 3% 0;
+      display: flex;
+      justify-content: space-between;
+
+      img {
+        width: 80%;
+      }
+
+    }
+    .hero {
+  padding-top: 8%;
+      h6 {
+        font-size: 25px;
+        font-weight: 100;
+        margin: 0;
+        letter-spacing: 2px;
+      }
+      h1 {
+        font-size: 150px;
+        margin: 0;
+        margin-top: -50px;
+        font-weight: 500;
+      }
+    }
+
+    .halo {
+      padding: 2% 0 0 0;
+      a {
+        color: #7842F0;
+        letter-spacing: 2px;
+        text-decoration: none;
+        border: 2px solid #7842F0;
+        border-radius: 35px;
+        padding: 1.5% 4%;
+        font-weight: 700;
+      }
+      a:hover {
+        background-color: #7842F0;
+        color: #fff;
+      }
+    }
+
+  }
+
+</style>
